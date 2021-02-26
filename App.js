551069e90e88129bf504cd560f19804782235e0d
Emma Mendez <emmamendez07@gmail.com>
2021-02-26 20:01:04 +0000

    logo added in place

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Contacts from 'expo-contacts';
+import { Gyroscope } from 'expo-sensors';
+
+
 
 export default function App() {
+console.log("Apped Up & Running");
+
   return (
     <View style={styles.container}>
       <Text>Log In</Text>
