c2fbe1f5b08557f5115d6b49003b3f42b1aa70c9
Emma Mendez <emmamendez07@gmail.com>
2021-03-03 14:00:07 +0000

    welcome page in place as  mvp

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image, ImageBackground, StyleSheet, View } from 'react-native';
+import { Button, Text } from 'native-base';
 
 function WelcomeScreen() {
     return (
@@ -8,8 +9,16 @@ function WelcomeScreen() {
           <Image
           style={styles.logo}
           source={require("../assets/logo512.png")} />
-          <View style={styles.loginButton}></View>
-          <View style={styles.registerButton}></View>
+          <View>
+          <Button  disabled block
+          style={styles.registerButton}>
+              <Text>Login</Text>
+          </Button>
+          <Button disabled block 
+          style={styles.loginButton}>
+              <Text>Register</Text>
+          </Button>
+          </View>
     </ImageBackground>
     );
 }
@@ -21,29 +30,27 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     loginButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: 'orange',
-        bottom: 100,
-
-    },
-    registerButton: {
-        width: '100%',
+        width: 200,
         height: 70,
         backgroundColor: 'orange',
         bottom: 260,
-
-        
+        borderRadius: 20,
+    },
+    registerButton: {
+        // width: '100%',
+        height: 70,
+        backgroundColor: 'orange',
+        bottom: 100,
+        borderRadius: 20,
+       
     },
     logo: {
         width: 200,
         height: 200,
         position: 'absolute',
         top: 70,
-
-
-
     },
+    
 })
 
 export default WelcomeScreen;
