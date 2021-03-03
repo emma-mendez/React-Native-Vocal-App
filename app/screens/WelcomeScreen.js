9684245504ffd8f030fd1c4b81cab612ed2dbdba
Emma Mendez <emmamendez07@gmail.com>
2021-03-03 12:16:14 +0000

    two buttons in place on welcome screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
-      <ImageBackground 
+    <ImageBackground 
       style={styles.background}
       source={require("../assets/logo512.png")}>
-
-      </ImageBackground>
+          <View style={styles.loginButton}></View>
+          <View style={styles.registerButton}></View>
+    </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     background: {
-        flex: 1
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: 'orange'
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: 'orange'
+        
     }
 })
 
