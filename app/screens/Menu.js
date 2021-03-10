ea6b260484df87f8f155be1aa3c8a2986e3e31ae
Emma Mendez <emmamendez07@gmail.com>
2021-03-10 13:22:34 +0000

    working welcome screen and moving pages

--- app/screens/Menu.js
+++ app/screens/Menu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Content, Text, Card, Header, Body, Button, Title, CardItem } from 'native-base';
+import { Image, ImageBackground, StyleSheet, View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 
@@ -9,35 +10,63 @@ export default class WelcomeScreen extends Component {
     
     return(
       <Container>
-        <Header>
-          <Body>
-            <Title>Menu</Title>
-          </Body>
-        </Header>
-        <Content padder>
-          <Card>
-            <CardItem>
-              <Body>
-                <Text>
-                  This is Page One, Press button to goto page two
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
-          <Button dark bordered style = {{aligniSelf: 'center', margin: 30}}
+        <ImageBackground 
+      style={styles.background}>
+          <Image
+          style={styles.logo}
+          source={require("../assets/logo512.png")} />
+          </ImageBackground>
+          <Button enable block 
+          style={styles.startButton}
             onPress= {() => {Actions.Menu2(); }}>
             <Text>Start</Text>
           </Button>
-          <Button dark bordered style = {{aligniSelf: 'center', margin: 30}}
-            onPress= {() => {Actions.Menu2(); }}>
+          <Button enable block
+          style={styles.loginButton}
+          onPress= {() => {Actions.Menu3(); }}>
             <Text>Login</Text>
           </Button>
-          <Button dark bordered style = {{aligniSelf: 'center', margin: 30}}
-            onPress= {() => {Actions.Menu2(); }}>
+          <Button enable block
+          style={styles.registerButton}
+            onPress= {() => {Actions.Menu4(); }}>
             <Text>Register</Text>
           </Button>
-         </Content>
       </Container>
+      
     );
   }
 }
+
+const styles = StyleSheet.create({
+  background: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center'
+  },
+  registerButton: {
+      height: 70,
+      backgroundColor: 'orange',
+      bottom: 70,
+      borderRadius: 20,
+  },
+  loginButton: {
+      height: 70,
+      backgroundColor: 'orange',
+      bottom: 90,
+      borderRadius: 20,
+  },
+  startButton: {
+      height: 70,
+      backgroundColor: 'orange',
+      bottom: 110,
+      borderRadius: 20,
+
+  },
+  logo: {
+      width: 200,
+      height: 200,
+      position: 'absolute',
+      top: 70,
+  },
+  
+})
