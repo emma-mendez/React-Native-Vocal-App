cff609ee5886e6bd61c997549feaed67cf0a1984
Emma Mendez <emmamendez07@gmail.com>
2021-03-10 14:41:50 +0000

    basic styling happening now working code good flow start of MVP

--- app/screens/Menu2.js
+++ app/screens/Menu2.js
@@ -1,35 +1,87 @@
 import React, { Component } from 'react';
 import { Container, Content, Text, Card, Header, Body, Button, Title, CardItem } from 'native-base';
 import { Actions } from 'react-native-router-flux';
+import { Image, ImageBackground, StyleSheet, View } from 'react-native';
+
+
+
+
 export default class pageTwo extends Component {
   render(){
     return(
       <Container>
         <Header>
-          <Body>
-            <Title>PageTwo</Title>
-          </Body>
+          
         </Header>
-        <Content padder>
-          <Card>
-            <CardItem>
-              <Body>
-                <Text>
-                  This is Page One, Press button to goto page two
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
-          <Button dark bordered
+          <Button enable block 
+          style={styles.singButton}
             onPress= {() => {Actions.pop(); }}>
-            <Text>Goto Page 1</Text>
+            <Text>Sing</Text>
          </Button>
-         <Button dark bordered style = {{alignSelf: 'center', margin: 30}}
-            onPress= {() => {Actions.Menu(); }}>
-            <Text>Log Out</Text>
+         <Button enable block 
+          style={styles.exitButton}
+          onPress= {() => {Actions.Menu(); }}>
+            <Text>Exit</Text>
           </Button>
-        </Content>
+          <Button enable block 
+          style={styles.nextButton}
+          onPress= {() => {Actions.Menu3(); }}>
+            <Text>Next</Text>
+          </Button>
+         <Button enable block 
+          style={styles.studioButton}
+          onPress= {() => {Actions.Menu(); }}>
+            <Text>Studio</Text>
+          </Button>
       </Container>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  background: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center'
+  },
+  singButton: {
+      margin: 40,
+      height: 70,
+      backgroundColor: 'orange',
+      bottom: 0,
+      borderRadius: 20,
+  },
+  exitButton: {
+      margin: 10,
+      height: 30,
+      width: 100,
+      backgroundColor: 'orange',
+      bottom: 0,
+      borderRadius: 20,
+  },
+  nextButton: {
+      margin: 10,
+      height: 30,
+      width: 100,
+      backgroundColor: 'orange',
+      bottom: 0,
+      borderRadius: 20,
+
+  },
+  studioButton: {
+    margin: 40,
+    height: 70,
+    backgroundColor: 'orange',
+    bottom: 0,
+    borderRadius: 20,
+
+},
+  logo: {
+      width: 200,
+      height: 200,
+      position: 'absolute',
+      top: 70,
+  },
+  
+})
