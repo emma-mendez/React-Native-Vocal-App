151080460f7ba00a6070c567e8093be95aee915a
harjeet <harjeet.kalsi@solirius.com>
2021-03-11 09:40:03 +0000

    Add inital code for sound wave test

--- app/screens/Journal.js
+++ app/screens/Journal.js
@@ -1,12 +1,82 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ImageBackground
+} from "react-native";
 
-export default function Journal() {
-    return (
-        <View>
-            <Text>Journal</Text>
-        </View>
-    )
+import { RNVoiceRecorder } from 'react-native-voice-recorder'
+let recordingPath;
+
+export default class Journal extends Component<Props> {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      visible: false
+    }
+  }
+
+  _onRecord() {
+    RNVoiceRecorder.Record({
+      format: 'wav',
+      onDone: (path) => {
+        console.log('record done: ' + path)
+
+        recordingPath = path;
+      },
+      onCancel: () => {
+        console.log('on cancel')
+      }
+    });
+  }
+
+  _onPlay() {
+    RNVoiceRecorder.Play({
+      path: recordingPath,
+      format: "wav",
+      onDone: path => {
+        console.log("play done: " + path);
+      },
+      onCancel: () => {
+        console.log("play cancelled");
+      }
+    });
+  }
+
+  render() {
+    return <ImageBackground source={require("../assets/background.jpg")} style={styles.backgroundImage}>
+      <Button onPress={() => {
+        this._onRecord()
+
+        // this.setState({ visible: true });
+      }} title={'Record'}>
+      </Button>
+      <Button onPress={() => {
+        this._onPlay()
+
+        // this.setState({ visible: true });
+      }} title={'Play'}>
+      </Button>
+    </ImageBackground>;
+  }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: null,
+    height: null,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
