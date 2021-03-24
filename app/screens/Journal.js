050571dba5682378c74c99f7c03a241957e240bf
harjeet <harjeet.kalsi@solirius.com>
2021-03-24 13:59:02 +0000

    Add changes

--- app/screens/Journal.js
+++ app/screens/Journal.js
@@ -1,67 +1,131 @@
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
-  Text,
-  View,
-  Button,
-  ImageBackground
 } from "react-native";
+import AudioRecorderPlayer, {
+ AVEncoderAudioQualityIOSType,
+ AVEncodingOption,
+ AudioEncoderAndroidType,
+ AudioSet,
+ AudioSourceAndroidType,
+} from 'react-native-audio-recorder-player';
 
-import { RNVoiceRecorder } from 'react-native-voice-recorder'
-let recordingPath;
+import { Avatar, Button, Card, Title, Divider, Paragraph } from 'react-native-paper';
+export default class Journal extends Component {
 
-export default class Journal extends Component<Props> {
   constructor(props) {
-    super(props)
 
+    super(props);
     this.state = {
-      visible: false
-    }
+      isLoggingIn: false,
+      recordSecs: 0,
+      recordTime: '00:00:00',
+      currentPositionSec: 0,
+      currentDurationSec: 0,
+      playTime: '00:00:00',
+      duration: '00:00:00',
+    };
+    this.audioRecorderPlayer = new AudioRecorderPlayer();
+    this.audioRecorderPlayer.setSubscriptionDuration(0.09); // optional. Default is 0.1
   }
 
-  _onRecord() {
-    RNVoiceRecorder.Record({
-      format: 'wav',
-      onDone: (path) => {
-        console.log('record done: ' + path)
-
-        recordingPath = path;
-      },
-      onCancel: () => {
-        console.log('on cancel')
-      }
+  onStartRecord = async () => {
+    const path = 'hello.m4a';
+    const audioSet = {
+      AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
+      AudioSourceAndroid: AudioSourceAndroidType.MIC,
+      AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
+      AVNumberOfChannelsKeyIOS: 2,
+      AVFormatIDKeyIOS: AVEncodingOption.aac,
+    };
+    console.log('audioSet', audioSet);
+    const uri = await this.audioRecorderPlayer.startRecorder(path, audioSet);
+    this.audioRecorderPlayer.addRecordBackListener((e) => {
+      this.setState({
+        recordSecs: e.current_position,
+        recordTime: this.audioRecorderPlayer.mmssss(
+            Math.floor(e.current_position),
+        ),
+      });
     });
-  }
+    console.log(`uri: ${uri}`);
+  };
 
-  _onPlay() {
-    RNVoiceRecorder.Play({
-      path: recordingPath,
-      format: "wav",
-      onDone: path => {
-        console.log("play done: " + path);
-      },
-      onCancel: () => {
-        console.log("play cancelled");
-      }
+  onStopRecord = async () => {
+    const result = await this.audioRecorderPlayer.stopRecorder();
+    this.audioRecorderPlayer.removeRecordBackListener();
+    this.setState({
+      recordSecs: 0,
     });
-  }
+    console.log(result);
+  };
+
+  onStartPlay = async (e) => {
+    console.log('onStartPlay');
+    const path = 'hello.m4a'
+    const msg = await this.audioRecorderPlayer.startPlayer(path);
+    this.audioRecorderPlayer.setVolume(1.0);
+    console.log(msg);
+    this.audioRecorderPlayer.addPlayBackListener((e) => {
+      if (e.current_position === e.duration) {
+        console.log('finished');
+        this.audioRecorderPlayer.stopPlayer();
+      }
+      this.setState({
+        currentPositionSec: e.current_position,
+        currentDurationSec: e.duration,
+        playTime: this.audioRecorderPlayer.mmssss(
+            Math.floor(e.current_position),
+        ),
+        duration: this.audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+      });
+    });
+  };
+
+  onPausePlay = async (e) => {
+    await this.audioRecorderPlayer.pausePlayer();
+  };
+
+  onStopPlay = async (e) => {
+    console.log('onStopPlay');
+    this.audioRecorderPlayer.stopPlayer();
+    this.audioRecorderPlayer.removePlayBackListener();
+  };
 
   render() {
-    return <ImageBackground source={require("../assets/background.jpg")} style={styles.backgroundImage}>
-      <Button onPress={() => {
-        this._onRecord()
-
-        // this.setState({ visible: true });
-      }} title={'Record'}>
+    return <Card
+        style={{flex: 1, flexDirection: 'row', alignItems: 'center', alignContent: 'center', alignSelf: 'center'}}>
+      <Title>{this.state.recordTime}</Title>
+      <Button mode="contained" icon="record" onPress={() => this.onStartRecord()}>
+        RECORD
       </Button>
-      <Button onPress={() => {
-        this._onPlay()
-
-        // this.setState({ visible: true });
-      }} title={'Play'}>
+      <Button
+          icon="stop"
+          mode="outlined"
+          onPress={() => this.onStopRecord()}
+      >
+        STOP
       </Button>
-    </ImageBackground>;
+      <Divider/>
+      <Title>{this.state.playTime} / {this.state.duration}</Title>
+      <Button mode="contained" icon="play" onPress={() => this.onStartPlay()}>
+        PLAY
+      </Button>
+      <Button
+          icon="pause"
+          mode="contained"
+          onPress={() => this.onPausePlay()}
+      >
+        PAUSE
+      </Button>
+      <Button
+          icon="stop"
+          mode="outlined"
+          onPress={() => this.onStopPlay()}
+      >
+        STOP
+      </Button>
+    </Card>
   }
 }
 
