9b2527f1e6ec6286cc4b359a06e69de04fb2f90b
Emma Mendez <emmamendez07@gmail.com>
2021-03-28 12:44:38 +0100

    working task componenet

---
+++ app/components/Task.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+export default class Task extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Check 1 2</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    
+})
--- app/screens/ToDo.js
+++ app/screens/ToDo.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Task from '../components/Task';
 
 export default function ToDo() {
     return (
-        <View>
-            <Text>To Do</Text>
+        <View stle={styles.container}>
+
+            {/* Todays Tasks */}
+        <View style={styles.taskWrapper}>
+            <Text style={styles.sectionTitle}></Text>
+            <Task />
+        <View style={styles.Items}>
+
+        </View>
+        
+        </View>
+
+            <Text>Todays Task</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+container:{
+    flex: 1,
+    backgroundColor: '#E8EAED',
+},
+taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+},
+sectionTitle:{
+    fontSize: 24,
+    fontWeight: 'bold'
+
+},
+Items:{},
+});
